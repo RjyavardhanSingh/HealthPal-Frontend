b526a314d40219f860c20338b981f79846f52d27
RajyavardhanSingh <rajyavardhansing2003@gmail.com>
2025-04-06 19:23:10 +0530

    fix broken api issue

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
+import api from '../../services/api';
 
 const Register = () => {
   const [formData, setFormData] = useState({
